ff91f32849ae8df3a4afaff9707c00a8e2161af5
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2024-03-03 23:38:58 +0100

    feat: util hooks added

--- src/shared/providers/App.provider.tsx
+++ src/shared/providers/App.provider.tsx
@@ -2,12 +2,14 @@
 
 import React, {FC, PropsWithChildren, ReactElement} from 'react';
 import AppThemeProvider from '@/features/theme/AppTheme.provider';
+import {ComposeChildren} from '@/lib/react';
 
 const AppProvider: FC<PropsWithChildren> = ({children}): ReactElement => {
   return (
-    <AppThemeProvider>
+    <ComposeChildren>
+      <AppThemeProvider/>
       {children}
-    </AppThemeProvider>
+    </ComposeChildren>
   );
 };
 
