689460cb7df4d55ac085c5117659bfb18ac1f012
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2024-04-01 18:49:20 +0200

    type fix

--- src/shared/api/signInSignUpAdmin.ts
+++ src/shared/api/signInSignUpAdmin.ts
@@ -1,22 +1,24 @@
-import {APIResponse} from '@/app/api/auth/APIResponse';
-import {UserCredential} from '@firebase/auth';
+import { APIResponse } from '@/app/api/auth/APIResponse';
+import { UserCredential } from '@firebase/auth';
 
-export const signInSignUpAdmin = async (userCredential: UserCredential): Promise<APIResponse<string>> => {
-  const idToken = await userCredential?.user.getIdToken();
+export const signInSignUpAdmin = async (
+	userCredential: UserCredential | undefined
+): Promise<APIResponse<string>> => {
+	const idToken = await userCredential?.user.getIdToken();
 
-  const response = await fetch('/api/auth/sign-in', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ idToken }),
-  });
+	const response = await fetch('/api/auth/sign-in', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ idToken }),
+	});
 
-  const resBody = (await response.json()) as unknown as APIResponse<string>;
+	const resBody = (await response.json()) as unknown as APIResponse<string>;
 
-  if (response.ok && resBody.success) {
-    return Promise.resolve(resBody);
-  }
+	if (response.ok && resBody.success) {
+		return Promise.resolve(resBody);
+	}
 
-  return Promise.reject(resBody);
+	return Promise.reject(resBody);
 };
