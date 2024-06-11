b513f66b62a7032ea7c629adc503fe87b7668c40
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2024-06-11 15:34:32 +0200

    edit: default user name is changed

--- src/shared/appConstants.ts
+++ src/shared/appConstants.ts
@@ -2,4 +2,4 @@ export const APP_NAME = 'Data meter keep';
 export const APP_DESCRIPTION =
   'In this application users can store their data from their meters and other relevant information linked to a specific date';
 
-export const DEFAULT_USER_DN = 'User';
+export const DEFAULT_USER_DN = 'Anonymous User';
